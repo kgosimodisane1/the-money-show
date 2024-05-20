64f18e498afdfcdbf1d82561d87ef2ae9300a42f
kgosimodisane1 <159646111+kgosimodisane1@users.noreply.github.com>
2024-05-20 23:07:12 +0200

    Update Coingecko API.py
    Pulled Bitcoin data from CoinGecko using API, and converted it into a readable table.

--- Coingecko API.py
+++ Coingecko API.py
@@ -1,4 +1,23 @@
-import pip
-import requests
 import pandas as pd
-from bs4 import beautifulsoup4
+import ast
+import requests
+
+# Pulling Bitcoin historical ohlc data @ 30-minute interval
+url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1&precision=2&x_cg_demo_api_key=CG-bQSUCXhXMtB9rRiMfYZN1BkY"
+
+headers = {"accept": "application/json"}
+
+# Storing pull request as a text
+response = requests.get(url, headers=headers)
+
+print(response.text)
+
+# Converting text to a data frame
+data_list = ast.literal_eval(response.text)
+
+df = pd.DataFrame(data_list, columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+
+# Converting date-time to a readable format
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='ms')
+
+print(df)

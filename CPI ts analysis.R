a9383f8fc84678211551c04bce67fc72345200f7
Kgosi Modisane <159646111+kgosimodisane1@users.noreply.github.com>
2024-06-15 21:33:16 +0200

    Update CPI ts analysis 1.2.R
    Pulled market share data for SA banks to create a banking industry index
    Converted price data to returns
    Plotted banks return performance as well as banking industry's return performance

--- CPI ts analysis.R
+++ CPI ts analysis.R
@@ -28,8 +28,74 @@ library(readxl)
 # - Investec Bank
 # - African Bank
 
+# Pulling stock data for South African banks
+
 getSymbols(c("CPI.JO", "ABG.JO", "SBKP.JO", "FSR.JO", "NED.JO", "INL.JO"), 
            from = Sys.Date()-740, 
-           to = Sys.Date())
+           to = Sys.Date()) #listed in ZARcents
 
+# Colleting Live Market Cap data for same stocks -> this is to understand market share
 
+SA_Banks_Mkt_Cap <- read_excel("SA Banks Market Cap.xlsx") #listed in ZARcents
+colnames(SA_Banks_Mkt_Cap) <- c("CPI", "ABG", "SBK", "FSR", "NED", "INL")
+
+Ttl_Banks_MktC <- sum(SA_Banks_Mkt_Cap)
+
+SA_Bank_Mkt_Share <- SA_Banks_Mkt_Cap/Ttl_Banks_MktC
+
+Banks_Prices <- cbind(CPI.JO$CPI.JO.Adjusted, ABG.JO$ABG.JO.Adjusted, 
+                      SBK.JO$SBK.JO.Adjusted, FSR.JO$FSR.JO.Adjusted, 
+                      NED.JO$NED.JO.Adjusted, INL.JO$INL.JO.Adjusted)
+colnames(Banks_Prices) <- c("CPI", "ABG", "SBK", "FSR", "NED", "INL")
+
+Banks_ret <- na.omit(
+  Return.calculate(
+    na.omit(Banks_Prices)
+  )
+)
+
+# There has to be a cleaner way to do the below code
+#Label this as an issue for Git
+
+RetxWeights <- Banks_ret
+RetxWeights <- Banks_ret$CPI * SA_Bank_Mkt_Share$CPI
+RetxWeights <- Banks_ret$ABG * SA_Bank_Mkt_Share$ABG
+RetxWeights <- Banks_ret$SBK * SA_Bank_Mkt_Share$SBK
+RetxWeights <- Banks_ret$FSR * SA_Bank_Mkt_Share$FSR
+RetxWeights <- Banks_ret$NED * SA_Bank_Mkt_Share$NED
+RetxWeights <- Banks_ret$INL * SA_Bank_Mkt_Share$INL
+
+SA_Bank_Index <- Banks_ret$CPI + Banks_ret$ABG + Banks_ret$SBK + 
+  Banks_ret$FSR + Banks_ret$NED + Banks_ret$INL
+colnames(SA_Bank_Index) <- c("Index")
+
+# Bank Performances
+
+## Cumulative returns
+
+cum_ret <- cumprod(1 + Banks_ret) -1 
+indexed_performance <- (cum_ret + 1) * 100
+
+ttl_cum_ret <- cumprod(1 + SA_Bank_Index) -1 
+ttl_indexed_performance <- (ttl_cum_ret + 1) * 100
+
+## plot
+
+c <- c("CPI" = "grey", "ABG" = "red", "SBK" = "royalblue", "FSR" = "gold", 
+       "NED" = "darkgreen", "INL" = "navy")
+
+ggplot(indexed_performance) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = CPI, col = "CPI"), size = 0.75) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = ABG, col = "ABG"), size = 0.75) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = SBK, col = "SBK"), size = 0.75) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = FSR, col = "FSR"), size = 0.75) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = NED, col = "NED"), size = 0.75) + 
+  geom_line(mapping = aes(x = index(indexed_performance), y = INL, col = "INL"), size = 0.75) +
+  scale_color_manual(values = c, guide = guide_legend(override.aes = list(fill = c))) +
+  labs(title = "Return Performance of South African Banks", x = "Time", y = "Return", color = "Legend") +
+  theme_classic() #High Correlation is Evident
+
+ggplot(as.data.frame(ttl_indexed_performance)) + 
+  geom_line(mapping = aes(x = index(SA_Bank_Index), y = Index), col = "black") + 
+  labs(title = "Return Performance for the Banking Industry", x = "Time", y = "Return") +
+  theme_classic() #CPI and FSR accounting for 48% of the market has reduced the performance of the banking industry

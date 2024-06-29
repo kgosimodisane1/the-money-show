4a8acc8267126e4a1f52a794f0cb99475726c29a
Kgosi Modisane <159646111+kgosimodisane1@users.noreply.github.com>
2024-06-29 22:49:28 +0200

    Update Portfolio Tracker 1.2.R

--- Portfolio Tracker.R
+++ Portfolio Tracker.R
@@ -35,3 +35,49 @@ ggplot(Port_perf) +
   scale_color_manual(values = col, guide = guide_legend(override.aes = list(fill = col))) +
   labs(title = "Investment Performance", x = "Time", y = "Rand Value", color = "Legend") +
   theme_classic()
+
+# Add a forecast to the graph
+
+#Principal = 17000
+#Annuity = 4000
+#Exp.Return = 0.25
+
+# init_date <- Sys.Date() - 3, -> "2024-06-25"
+Days <- seq(from = init_date, to = Sys.Date() - 2, by = "day")
+
+obs <- as.numeric(Sys.Date() - init_date - 1)
+
+Principal <-  17000
+Annuity <-  4000/30 #monthly annuity divided by days in a month
+Exp_ret <-  0.15/252
+
+create_sequence <- function(base, growth_rate, n, add_ons) {
+  # Initialize the sequence vector with the base value
+  sequence <- numeric(n)
+  sequence[1] <- base
+  
+  # Initial increment
+  increment <- 0
+  
+  # Fill in the sequence with the growing increment
+  for (i in 2:n) {
+    sequence[i] <- sequence[i-1] + (sequence[i-1] * growth_rate) + increment
+    increment <- increment + add_ons
+  }
+  
+  return(sequence)
+}
+
+Exp_perf <- create_sequence(Principal, Exp_ret, obs, Annuity)
+#colnames(Exp_perf) <- c("Benchmark")
+
+Exp_perf_ts <- data.frame(Days, Exp_perf)
+
+col2 <- c("Portfolio" = "black", "Benchmark" = "turquoise")
+
+ggplot() +
+  geom_line(data = Port_perf, mapping = aes(x = index(Port_perf), y = Ttl_value, col = "Portfolio"), linewidth = 0.75) +
+  geom_line(data = Exp_perf_ts, mapping = aes(x = Days, y = Exp_perf, col = "Benchmark"), linewidth = 0.75) +
+  scale_color_manual(values = col2, guide = guide_legend(override.aes = list(fill = col2))) +
+  labs(title = "Portfolio Performance vs Benchmark", x = "Time", y = "Rand Value", color = "Legend") +
+  theme_classic()

471ff013f4423ef94f794f92e46e20f9bbee25c6
Kgosi Modisane <159646111+kgosimodisane1@users.noreply.github.com>
2024-09-24 15:06:20 +0200

    Update Portfolio Tracker 1.2.R

--- Portfolio Tracker.R
+++ Portfolio Tracker.R
@@ -36,6 +36,36 @@ ggplot(Port_perf) +
   labs(title = "Investment Performance", x = "Time", y = "Rand Value", color = "Legend") +
   theme_classic()
 
+#Portfolio Return Perfomance
+
+latest_date <- Sys.Date()-1
+
+ret_perf <- rportf[paste(init_date,latest_date, sep = "::")]
+
+cum_ret_perf <- cumprod(1 + ret_perf) -1 #Th e object `ret_perf` is sourced from "Optimisation uing fPortfolio" script.
+cum_ret_perf <- (cum_ret_perf + 1) * 100
+
+ggplot(cum_ret_perf) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = INL, col = "INL"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = STX500, col = "STX500"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = STX40, col = "STX40"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = STX100, col = "STX100"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = STXEMG, col = "STXEMG"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = SYGWD, col = "SYGWD"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = PAH3, col = "PAH3"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = CPI, col = "CPI"), linewidth = 0.75) +  
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = PPE, col = "PPE"), linewidth = 0.75) +  
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = SDO, col = "SDO"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = COH, col = "COH"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = SUI, col = "SUI"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = RIVN, col = "RIVN"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = BTC, col = "BTC"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = ETH, col = "ETH"), linewidth = 0.75) +
+  geom_line(mapping = aes(x = index(cum_ret_perf), y = UNI, col = "UNI"), linewidth = 0.75) +
+  scale_color_manual(values = col, guide = guide_legend(override.aes = list(fill = col))) +
+  labs(title = "Return Performance", x = "Time", y = "Return", color = "Legend") +
+  theme_dark()
+
 # Add a forecast to the graph
 
 #Principal = 17000
